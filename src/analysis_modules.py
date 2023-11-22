3f716ae2a65a0e6afed3ffcd7049a558213f54e3
jackson <jacksonlcrowley@gmail.com>
2023-11-22 17:16:55 +0100

    first version of parallel analysis

--- src/analysis_modules.py
+++ src/analysis_modules.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 import MDAnalysis as mda
 from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from functools import partial
 
 def non_water_COG(universe):
     """
@@ -51,6 +53,7 @@ def lipids_per_tubule_leaflet(universe, axis="z", residue_atom_dict=False, cente
     last atoms, assign each residue (i.e. lipid) to either the inner or outer tubule leaflet,
     where the tubule is periodic in the dimension specified by variable 'axis'
     """
+
     if residue_atom_dict == False:
         residue_atom_dict = first_and_last_atoms(universe, residue_names(universe.select_atoms("not resname W NA CA CL")))
     
@@ -91,6 +94,54 @@ def lipids_per_tubule_leaflet(universe, axis="z", residue_atom_dict=False, cente
         overall_totals.append(inner_total)
     return(overall_totals)
 
+def lipids_per_tubule_leaflet_parallel(frame_index, universe, axis="z"):
+    """
+    From an MDAnalysis universe and a dictionary containing residue names with their first and 
+    last atoms, assign each residue (i.e. lipid) to either the inner or outer tubule leaflet,
+    where the tubule is periodic in the dimension specified by variable 'axis'
+    """
+
+    universe.universe.trajectory[frame_index]
+
+    residue_atom_dict = first_and_last_atoms(universe, residue_names(universe.select_atoms("not resname W NA CA CL")))
+
+    center = non_water_COG(universe)
+
+    assert axis in ["x", "y", "z"], "axis is not one of 'x', 'y', or 'z'"
+    assert all(isinstance(key, str) and isinstance(value, list) and len(value) == 2 and all(isinstance(item, str) for item in value) for key, value in residue_atom_dict.items()), "Invalid dictionary format"
+
+    if axis == "x":
+        tubule_center = [center[1], center[2]]
+        dims = [1,2]
+    elif axis == "y":
+        tubule_center = [center[0], center[2]]
+        dims = [0,2]
+    elif axis == "z":
+        tubule_center = [center[0], center[1]]
+        dims = [0,1]
+    
+    overall_totals = []
+
+    for resname in residue_atom_dict:
+        heads = universe.select_atoms(f'resname {resname} and name {residue_atom_dict[resname][0]}')
+        tails = universe.select_atoms(f'resname {resname} and name {residue_atom_dict[resname][1]}')
+
+        outer_total = 0
+        inner_total = 0
+        for i in range(len(heads)):
+            head_coords = [heads.positions[i][dims[0]], heads.positions[i][dims[1]]]
+            tail_coords = [tails.positions[i][dims[0]], tails.positions[i][dims[1]]]
+            head_dist = (distance_between_points(head_coords, tubule_center))
+            tail_dist = (distance_between_points(tail_coords, tubule_center))
+            if head_dist > tail_dist:
+                outer_total += 1
+            else:
+                inner_total += 1
+        overall_totals.append(outer_total)
+        overall_totals.append(inner_total)
+    return(overall_totals)
+
+
 def results_to_df(array, resnames):
     """
     Take an array and list of resnames. For each resname, make a column for both Outer and Inner
@@ -121,6 +172,22 @@ def process_trajectory(trajectory, skip=1, output="dataframe.csv", axis = "z"):
     df = results_to_df(trajectory_output, resnames)
     df.to_csv(output)
 
+def process_trajectory_parallel(universe,  output="dataframe.csv", axis = "z"):
+    """
+    Perform inner/outer tubule analysis on entire trajectory, given here
+    as MDAnalysis universe. Saves as a csv named by variable 'output'
+    """
+    resnames = residue_names(universe.select_atoms("not resname W"))
+
+    run_per_frame = partial(lipids_per_tubule_leaflet_parallel, universe=universe, axis=axis)
+
+    frame_values = np.arange(universe.trajectory.n_frames)
+
+    with Pool(25) as worker_pool:
+        result = worker_pool.map(run_per_frame, frame_values)
+
+    df = results_to_df(result, resnames)
+    df.to_csv(output)
 
 def df_to_plot(df, resnames, rolling=1, title=False, colours=False, x_label="Frame"):
     """
