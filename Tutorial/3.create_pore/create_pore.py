4bb2c481791ce7eae38bfed8c9f4cb7d677a32ac
jackson <jacksonlcrowley@gmail.com>
2024-02-01 11:23:28 +0100

    Some tutorial work

--- Tutorial/3.create_pore/create_pore.py
+++ Tutorial/3.create_pore/create_pore.py
@@ -1,4 +1,5 @@
-from src import pore_modules, analysis_modules
+import os
+from src import pore_modules
 import argparse
 
 parser = argparse.ArgumentParser(description = "Take an input gro structure, from which you can select multiple residues using -r. These residues will have restraint coordinates for x, y and z written. These are written at 0 by default, but this can be changed using the -x, -y and -z flags.")
@@ -6,10 +7,11 @@ parser = argparse.ArgumentParser(description = "Take an input gro structure, fro
 ## generation of pore
 parser.add_argument("-c", "--input", help = "<.gro> input structure file", required = True)
 parser.add_argument("-o", "--output", help = "<.gro> desired output .gro file (default = 'output.gro') (opt.)", default = "output.gro")
-parser.add_argument("-r", "--radius", default=2)
+parser.add_argument("-r", "--radius", type=float, default=2)
 parser.add_argument("--residues", nargs="*", type=str, help = "<str> residues to restrain (call multiple times if needed)", required = True)
 parser.add_argument("-a", "--axis", help = "pore axis", default = "x")
 parser.add_argument("--coords", help = "3-dimensional list of integers or floats for precising pore location. If not specified, the coordinates are set in the center of the box")
+parser.add_argument("--renumber", type=bool, default=True, help="Sort the residues such that residues of the same type are grouped; set to True by default as it causes conflicts with the (current version of) topology rewriting. Gro files created in TS2CG are ordered by residue and leaflet, unfortunately we lose this specificity here.")
 
 ## generate new topology
 parser.add_argument("-p", "--topology", help = "<.top> input topology file")
@@ -33,6 +35,9 @@ else:
 
 # 1. Generate Pore
 pore_modules.create_pore(args.input, args.output, args.axis, args.radius, args.residues, centerXYZ)
+if args.renumber == True: 
+    pore_modules.reorder_gro(args.output, pore_modules.mol_list(args.output))
+    os.rename("renumbered.gro", args.output)
 
 # 2. Generate Topology
 if args.topology:
