940c72a8945fb6b8b05cce09d9046034bdbdfda3
jackson <jacksonlcrowley@gmail.com>
2024-02-01 14:01:23 +0100

    submodule update

--- Tutorial/1.initial/work/0e/267f3fa51116b20634b5d712d7fc22/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-PCG -str generate.str -Bondlength  0.2  -LLIB /data2/jackson/lipid_sorting/lipid_sorting_in_tubules/TS2CG-Setup-Pipeline/top/Martini3+NLs.LIB -function  analytical_shape
-rm pcg.log
-
-cat header.txt > system.top
-cat output.top >> system.top
--- Tutorial/1.initial/work/24/8619213cc483bf3173e844d705ef0e/.command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ue
-PCG -str generate.str -Bondlength  0.2  -LLIB /data1/jackson/TS2CG_pipeline/top/Martini3+NLs.LIB -function  analytical_shape
-rm pcg.log
-
-cat header.txt > system.top
-cat output.top >> system.top
-
-sed -i "s/PATH//data1/jackson/TS2CG_pipeline/g" system.top
